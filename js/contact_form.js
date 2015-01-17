4c37952b45185ba6d4b49aa06e438484e2c6f0e4
Thibaut LE LEVIER <thibaut@lelevier.fr>
2015-01-17 16:44:41 +0100

    add some validation on the contact form

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,6 +1,21 @@
 $(function() {
     $("#contact_form").submit(function(e) {
       e.preventDefault();
+
+      if (! $("[name='_replyto']").val())
+      {
+        alert("Your email is mandatory if you want me to get in touch!");
+        $("#your_email").css("border", "1px solid #ff0000");
+        return;
+      }
+
+      if (! $("[name='message']").val())
+      {
+        alert("Tell me about the purpose of your message and how I can help you!");
+        $("#your_message").css("border", "1px solid #ff0000");
+        return;
+      }
+
       $.ajax({
         url: "//formspree.io/thibaut@lelevier.com", 
         method: "POST",
